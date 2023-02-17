aa3ef12424b7871e604605a1c3612b6225dcc6f3
maharun0 <coderbeast7@gmail.com>
2023-02-17 13:06:53 +0600

    LOJ-1000

---
+++ LightOJ/Warm_Up/Greetings_from_LightOJ.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n"
+#define suii ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int main()
+{
+    suii
+    int t, a, b;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++){
+        
+        cin >> a >> b;
+        cout << "Case " << tc << ": " << a + b << endl;
+    }
+
+    
+    return 0;
+}
